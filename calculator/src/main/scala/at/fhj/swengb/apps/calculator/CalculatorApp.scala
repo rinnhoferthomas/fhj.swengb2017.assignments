61e9981de2591f62701eeffac751b533b71325e3
rinnhoferthomas <thomas.rinnhofer@edu.fh-joanneum.at>
2017-12-11 18:43:05 +0100

    Merge remote-tracking branch 'remotes/upstream/master'
    # Conflicts:
    #	calculator/src/test/scala/at/fhj/swengb/apps/calculator/TimesheetSpec.scala

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/CalculatorApp.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/CalculatorApp.scala
@@ -64,11 +64,83 @@ class CalculatorFxController extends Initializable {
 
   def sgn(): Unit = {
     getCalculator().push(Op(numberTextField.getText)) match {
-      case Success(c) => setCalculator(c)
-      case Failure(e) => // show warning / error
+      case Success(c) => setCalculator(c); numberTextField.setText(c.peek().asInstanceOf[Val].value.toString)
+      case Failure(e) => setCalculator(RpnCalculator()); numberTextField.setText("Error") // show warning / error
     }
     getCalculator().stack foreach println
   }
 
+  def btn0Clk(): Unit = setNumber("0")
+
+  def btn1Clk(): Unit  = setNumber("1")
+
+  def btn2Clk(): Unit  = setNumber("2")
+
+  def btn3Clk(): Unit  = setNumber("3")
+
+  def btn4Clk(): Unit  = setNumber("4")
+
+  def btn5Clk(): Unit  = setNumber("5")
+
+  def btn6Clk(): Unit  = setNumber("6")
+
+  def btn7Clk(): Unit  = setNumber("7")
+
+  def btn8Clk(): Unit  = setNumber("8")
+
+  def btn9Clk(): Unit = setNumber("9")
+
+  def btnMulClk(): Unit = setOp("*")
+
+  def btnDivClk(): Unit = setOp("/")
+
+  def btnPlsClk(): Unit = setOp("+")
+
+  def btnMinClk(): Unit = setOp("-")
+
+  def btnPtnClk(): Unit = {
+    if (numberTextField.getText != "Error" && !numberTextField.getText.contains(".")) {
+      numberTextField.setText(numberTextField.getText + ".")
+    }
+  }
+
+  def btnSgnClk(): Unit = {
+    if (numberTextField.getText != "Error"
+      && numberTextField.getText != "0"
+      && numberTextField.getText != "*"
+      && numberTextField.getText != "/"
+      && numberTextField.getText != "-"
+      && numberTextField.getText != "+") {
+      val dbl = numberTextField.getText.toDouble * -1
+      numberTextField.setText(dbl.toString)
+    }
+  }
+
+  def btnCClk(): Unit = {
+    if (numberTextField.getText == "0") {
+      setCalculator(RpnCalculator())
+    } else {
+      numberTextField.setText("0")
+    }
+  }
+
+  private def setOp(op: String): Unit = {
+    if (numberTextField.getText != "Error") {
+      numberTextField.setText(op)
+    }
+  }
+
+  private def setNumber(s: String): Unit = {
+    if (numberTextField.getText == "Error"
+      || numberTextField.getText == "0"
+      || numberTextField.getText == "*"
+      || numberTextField.getText == "/"
+      || numberTextField.getText == "-"
+      || numberTextField.getText == "+") {
+      numberTextField.setText(s)
+    } else {
+      numberTextField.setText(numberTextField.getText + s)
+    }
+  }
 
 }
--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
@@ -1,6 +1,6 @@
 package at.fhj.swengb.apps.calculator
 
-import scala.util.Try
+import scala.util.{Try, Success, Failure}
 
 /**
   * Companion object for our reverse polish notation calculator.
@@ -14,8 +14,25 @@ object RpnCalculator {
     * @param s a string representing a calculation, for example '1 2 +'
     * @return
     */
-  def apply(s: String): Try[RpnCalculator] = ???
+  def apply(s: String): Try[RpnCalculator] = {
+    if (s == "") {
+      return Success(RpnCalculator())
+    }
 
+    var controller = RpnCalculator()
+
+    for (str <- s.split(" ")) {
+      val op = Op(str)
+
+      if (controller.push(op).isSuccess) {
+        controller = controller.push(op).get
+      } else {
+        return controller.push(op)
+      }
+    }
+
+    return Success(controller)
+  }
 }
 
 /**
@@ -32,7 +49,26 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Op): Try[RpnCalculator] = Try(RpnCalculator())
+  def push(op: Op): Try[RpnCalculator] = {
+    def operate(v: (Val, Val) => Val): Try[RpnCalculator]= try {
+      val (last, lastC) = pop
+      val (bLast, bLastC) = lastC.pop
+      (last, bLast) match {
+        case (left: Val, right: Val) => bLastC.push(v(left, right))
+        case _ => Failure(new NoSuchElementException)
+      }
+    } catch {
+      case e: NoSuchElementException => Failure(e)
+    }
+
+    op match {
+      case v: Val => Success(RpnCalculator(v :: stack))
+      case Mul => operate(Mul.eval)
+      case Sub => operate(Sub.eval)
+      case Add => operate(Add.eval)
+      case Div => operate(Div.eval)
+    }
+  }
 
   /**
     * Pushes val's on the stack.
@@ -42,26 +78,40 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @param op
     * @return
     */
-  def push(op: Seq[Op]): Try[RpnCalculator] = ???
+  def push(op: Seq[Op]): Try[RpnCalculator] = {
+    op match {
+      case h :: Nil => push(h)
+      case h :: t => push(h) match {
+        case Success(c) => c.push(t)
+        case Failure(e) => Failure(e)
+      }
+    }
+  }
 
   /**
     * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
     *
     * @return
     */
-  def pop(): (Op, RpnCalculator) = ???
+  def pop(): (Op, RpnCalculator) = (peek(), RpnCalculator(stack.take(stack.length - 1)))
 
   /**
     * If stack is nonempty, returns the top of the stack. If it is empty, this function throws a NoSuchElementException.
     *
     * @return
     */
-  def peek(): Op = ???
+  def peek(): Op = {
+    if (stack.nonEmpty) {
+      stack.last
+    } else {
+      throw new NoSuchElementException
+    }
+  }
 
   /**
     * returns the size of the stack.
     *
     * @return
     */
-  def size: Int = stack.length
+  def size: Int = stack.count(_.isInstanceOf[Val])
 }
